fc30276232e74edb71b1591d1dc8b3c8ba8778c7
VitorOM2 <viktormarque2053@gmail.com>
2022-07-28 10:28:37 -0300

    Agora as teclas tocam seus devidos sons

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -7,9 +7,15 @@ const listaTeclas = document.querySelectorAll('.tecla');
 let contador = 0;
 
 while (contador < listaTeclas.length){
-    listaTeclas[contador].onclick = tocaSom;
+    
+    const tecla = listaTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    }
 
     contador++;
 
-    console.log(contador)
 }

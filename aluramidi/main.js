1274f180552c9f29167f4ceeea2a9f02523fe58b
VitorOM2 <viktormarque2053@gmail.com>
2022-07-28 11:52:17 -0300

    Adicionada funções para funcionalida de teclas

--- aluramidi/main.js
+++ aluramidi/main.js
@@ -16,4 +16,15 @@ for (let contador = 0; contador < listaTeclas.length; contador++){
         tocaSom(idAudio);
     }
 
+    tecla.onkeydown = function(evento){
+
+        if(evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('ativa');
+        }
+
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
 }
